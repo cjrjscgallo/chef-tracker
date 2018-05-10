a2fb149ee1c46e6ba871880633b2222c26e9a5c3
Chris Gallo <cgallo@fnba.com>
2018-05-10 11:48:23 -0400

    exercise 3

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,11 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  name: `chef`
+  chefs: [
+    {name: 'Jenny', isCooking: true},
+    {name: 'Mom', isCooking: true},
+    {name: 'Oma', isCooking: true},
+    {name: 'Me', isCooking: true},
+    {name: 'Grandma Glawe', isCooking: true}
+  ]
 });
